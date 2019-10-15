0d2c64cb4c6c1b04f58e58aa7ac696d8ee674d85
baptiste-pajot <bpajot@student.42.fr>
2019-10-15 12:25:15 +0200

    manage triangle indice

--- srcs/scop.c
+++ srcs/scop.c
@@ -6,7 +6,7 @@
 /*   By: bpajot <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 11:30:49 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/15 11:28:02 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/15 12:24:05 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -43,49 +43,81 @@ static const char	*ft_fragment_shader(void)
 	return ("#version 410\n"
 	"out vec4 frag_colour;\n"
 	"void main() {\n"
-	"  frag_colour = vec4(1, 1, 1, 1);\n"
+	"  frag_colour = vec4(1.0, 1.0, 1.0, 1.0);\n"
 	"}\n");
 }
 
 static void			display(void)
 {
-	float			vertex[] = {
-		+0.0, +0.5, +0.0,
-		-0.5, -0.5, +0.0,
+	GLfloat			vertices[] = {
+		-0.5, +0.5, +0.0,
+		+0.5, +0.5, +0.0,
 		+0.5, -0.5, +0.0,
-		+0.0, +0.8, +0.0,
-		-0.5, +0.7, +0.0,
-		+0.5, +0.7, +0.0
+		-0.5, -0.5, +0.0,
+		-0.6, +0.4, +0.0,
 	};
-	GLuint			vbo;
+
+	GLuint			indices[] = {
+		0, 1, 2,
+		2, 3, 4
+	};
+
 	GLuint			vao;
+	GLuint			vbov;
+	GLuint			vboi;
+
 	const char		*txt_vertex_shader = ft_vertex_shader();
 	const char		*txt_fragment_shader = ft_fragment_shader();
 
-	glGenBuffers(1, &vbo); //generate Vertex Buffer Object
-	glBindBuffer(GL_ARRAY_BUFFER, vbo); //activate the VBO
-	//copy data to VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+	// Generate and activate the Vertex Array Object
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// Generate and activate the Vertex Buffer Object Vertices
+	glGenBuffers(1, &vbov);
+	glBindBuffer(GL_ARRAY_BUFFER, vbov);
+	// Copy data to VBO Vertices
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Generate and activate the Vertex Buffer Object Indices
+	glGenBuffers(1, &vboi);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboi);
+	// Copy data to VBO Indices
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+			GL_STATIC_DRAW);
+
+	// Create and compile the Vertex Shader
 	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vs, 1, &txt_vertex_shader, NULL);
 	glCompileShader(vs);
+
+	// Create and compile the Fragment Shader
 	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fs, 1, &txt_fragment_shader, NULL);
 	glCompileShader(fs);
+
+	// Link the Vertex and the Fragment Shaders
 	GLuint shader_programme = glCreateProgram();
 	glAttachShader(shader_programme, fs);
 	glAttachShader(shader_programme, vs);
 	glLinkProgram(shader_programme);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shader_programme);
-	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDrawArrays(GL_TRIANGLES, 3, 3);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// Clear the screen to black
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glDeleteProgram(shader_programme);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	glDeleteBuffers(1, &vbov);
+	glDeleteBuffers(1, &vboi);
+	glDeleteVertexArrays(1, &vao);
 }
 
 int					main(void)
